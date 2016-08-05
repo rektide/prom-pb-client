22b6a1fce2aea34889c24298caff2b768694bd43
rektide <rektide@voodoowarez.com>
2016-08-04 23:57:15 -0400

    Elaborate content-type. Add a Find utility.

--- content-type.js
+++ content-type.js
@@ -6,6 +6,66 @@ var
   compact= "application/vnd.google.protobuf; proto=io.prometheus.client.MetricFamily; encoding=compact-text"
 
 module.exports= new String(delimited)
-module.exports.delimited= delimited
-module.exports.text= text
-module.exports.compact= compact
+Object.defineProperties(module.exports, { // non-visible "meta"-info
+	'@type': {
+		value: "application/vnd.google.protobuf"
+	},
+	proto: {
+		value: "io.prometheus.client.MetricFamily"
+	},
+	encoding: {
+		value: "delimited"
+	}
+})
+module.exports.delimited= module.exports
+module.exports.text= new String(text)
+Object.defineProperties(module.exports.text, {
+	'@type':{
+		value: "application/vnd.google.protobuf"
+	},
+	proto: {
+		value: "io.prometheus.client.MetricFamily"
+	},
+	encoding: {
+		value: "text"
+	}
+})
+module.exports.compactText= new String(compact)
+Object.defineProperties(module.exports.compactText, {
+	'@type':{
+		value: "application/vnd.google.protobuf"
+	},
+	proto:{
+		value: "io.prometheus.client.MetricFamily"
+	},
+	encoding:{
+		value: "compact-text"
+	}
+})
+
+
+// for a given content type, find matches
+function find( type, quals){
+	if( type!= "application/vnd.google.protobuf"&& quals.proto!= "io.prometheus.client.MetricFamily"){
+		return
+	}
+	for( var e in module.exports){
+		var encoding= module.exports[ e]
+		if( encoding.encoding== quals.encoding){
+			return encoding
+		}
+	}
+}
+
+// non-visible alias
+Object.defineProperties( module.exports,{
+	"compact-text":{
+		value: module.exports.compact,
+		enumerable: false
+	},
+	find:{
+		value: find,
+		enumerable: false
+	}
+})
+
